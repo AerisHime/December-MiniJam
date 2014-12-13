e43ba5c4432580fda55a68edd08336e6a081795e
AerisHime <kristina.rothe@live.com>
2014-12-13 17:41:26 +0100

    Added functionality

---
+++ Assets/Scripts/FixedRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.zero, Vector3.zero);
+	}
+}
