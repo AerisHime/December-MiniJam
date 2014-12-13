53c30f8548496ebfe125c345b5a303e682cdd6cf
AerisHime <kristina.rothe@live.com>
2014-12-13 16:02:55 +0100

    Session activity
    To determine what to do when the game is running and when it isn't (i.e.
    stop score and such)

---
+++ Assets/Scripts/GameControls/SessionActive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SessionActive : MonoBehaviour {
+
+    bool gameRunning = true;
+    // Use this for initialization
+
+    public void Running()
+    {
+        gameRunning = true;
+    }
+
+    public void StopRunning()
+    {
+        gameRunning = false;
+    }
+
+    public bool CheckState()
+    {
+        return gameRunning;
+    }
+
+}
--- Assets/Scripts/ScoreSystem/MainScore.cs
+++ Assets/Scripts/ScoreSystem/MainScore.cs
@@ -5,19 +5,23 @@ using System.Collections;
 public class MainScore : MonoBehaviour {
 
     public Text ScoreText;
-    
+    SessionActive sessionActivity;
     public float Score = 10000;
     public float decreaserate = 50;
 	// Use this for initialization
 	void Start () {
-	
+        sessionActivity = this.GetComponent<SessionActive>();
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        
-        Score -= decreaserate*Time.deltaTime;
-        ScoreText.text = Score.ToString();
+        if (sessionActivity.CheckState())
+        {
+            Score -= decreaserate * Time.deltaTime;
+            ScoreText.text = Score.ToString();
+            
+        }
 
 	}
 }
