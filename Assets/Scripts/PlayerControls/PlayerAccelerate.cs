e02e7bf0fb4c64c163d0ca54bf76558597e3be7c
AerisHime <kristina.rothe@live.com>
2014-12-13 14:57:38 +0100

    player acceleration and deceleration

--- Assets/Scripts/PlayerControls/PlayerAccelerate.cs
+++ Assets/Scripts/PlayerControls/PlayerAccelerate.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class PlayerAccelerate : MonoBehaviour {
 
+    public Vector2 speedUp = new Vector2(250f, -250f);
 	// Use this for initialization
 	void Start () {
 	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+       
+        //for testing acceleration
+        if (Input.GetKeyDown("d")) 
+        {
+            Debug.Log("Hit button");
+
+            rigidbody2D.AddForce(speedUp, ForceMode2D.Impulse);
+        }
 	}
+    void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        if (otherObj.gameObject.tag == "Faster")
+        {
+            this.rigidbody2D.AddForce(speedUp, ForceMode2D.Impulse);
+
+            Destroy(otherObj.gameObject);
+        }
+    }
+
+
 }
--- Assets/Scripts/PlayerControls/PlayerSlow.cs
+++ Assets/Scripts/PlayerControls/PlayerSlow.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerSlow : MonoBehaviour {
 
+    public Vector2 speedDown = new Vector2(-400f, 400f);
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,22 @@ public class PlayerSlow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+       //for testing the deceleration
+        if (Input.GetKeyDown("a"))
+        {
+            Debug.Log("Hit button");
+
+            rigidbody2D.AddForce(speedDown, ForceMode2D.Impulse);
+        }
 	
 	}
+    void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        if (otherObj.gameObject.tag == "Slower")
+        {
+            this.rigidbody2D.AddForce(speedDown, ForceMode2D.Impulse);
+
+            Destroy(otherObj.gameObject);
+        }
+    }
 }
