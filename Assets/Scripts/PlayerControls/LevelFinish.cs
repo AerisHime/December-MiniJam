bbc386ec8e4adba6e6448c8865637f1ec82aaa52
mshbeab <mshbeab@gmail.com>
2014-12-13 18:25:38 +0100

    Added LevelFinish Scsript

---
+++ Assets/Scripts/PlayerControls/LevelFinish.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelFinish : MonoBehaviour {
+	SessionActive sessionActivity;
+	public GameObject GameController ;
+	
+	// Use this for initialization
+	void Start () {
+		sessionActivity = GameController.GetComponent<SessionActive> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("vilocity" +rigidbody2D.velocity.ToString()) ;
+	}
+	
+	void OnTriggerEnter2D(Collider2D otherObj)
+	{
+		if (otherObj.gameObject.tag == "Finish")
+		{	
+			//this.sessionActivity.StopRunning() ;
+			// Play Finish Animation 
+			sessionActivity.StopRunning() ;
+			Time.timeScale = 0.0f ;
+			
+		}
+	}
+}
+
