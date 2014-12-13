4cf9e79444f8c7d4e50e3ec22c56d7bcd9e626ef
AerisHime <kristina.rothe@live.com>
2014-12-13 14:06:38 +0100

    Jump Restrictions

--- Assets/Scripts/PlayerControls/PlayerJump.cs
+++ Assets/Scripts/PlayerControls/PlayerJump.cs
@@ -6,16 +6,27 @@ public class PlayerJump : MonoBehaviour {
     public Vector2 jumpHeight;
     bool canJump;
 	void Start () {
-	
+        canJump = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButtonDown(0))
+        if (canJump && Input.GetMouseButtonDown(0))
         {
-            
+            rigidbody2D.velocity = jumpHeight;
         }
 
 	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        canJump = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        canJump = false;
+    }
+
 }
